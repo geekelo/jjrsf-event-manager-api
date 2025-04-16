0b2c49183fa61b827aa11f2aabb44cfeb08aaccb
Eloghene <eloghene@bananacrystal.com>
2025-04-16 09:22:22 +0100

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  namespace :api do
+    namespace :v1 do
+      post "signup" => "registration#create"
+      post "login" => "authentication#create"
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
