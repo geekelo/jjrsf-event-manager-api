ed185a9de1a40f4bba898cae4cb1afd790de2bfc
Eloghene <eloghene@bananacrystal.com>
2025-04-16 22:12:07 +0100

    Added status to the permited params when creating event

--- app/controllers/api/v1/foundation_events_controller.rb
+++ app/controllers/api/v1/foundation_events_controller.rb
@@ -53,6 +53,6 @@ class Api::V1::FoundationEventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:name, :description, :start_date, :end_date, :online, :onsite, :location, :registration_deadline) 
+    params.require(:event).permit(:name, :description, :start_date, :end_date, :status, :online, :onsite, :location, :registration_deadline) 
   end
 end
