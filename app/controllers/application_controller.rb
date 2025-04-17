d14574a3b27d92ef74b92aa7fa1e6a6d6c6f8b0b
Eloghene <eloghene@bananacrystal.com>
2025-04-17 15:20:32 +0100

    Refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::API
     payload = JsonWebToken.decode(auth_token)
     @current_user = EventUser.find(payload['sub'])
   rescue JWT::DecodeError
-    render json: { error: 'Invalid Auth Token' }, status: :unauthorized
+    render json: { error: 'Invalid auth token' }, status: :unauthorized
   end
 
   def auth_token
-    @auth_token ||= request.headers.fetch('Authorization', '').split.last
-  end
+    @auth_token ||= request.headers['Authorization'].to_s.split.last
+  end  
 end
