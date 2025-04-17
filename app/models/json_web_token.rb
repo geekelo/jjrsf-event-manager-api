d95596404669d28472fc41cdb7384c7d8046f11f
Eloghene <eloghene@bananacrystal.com>
2025-04-17 15:25:19 +0100

    Trouble-shooting

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,10 +1,13 @@
 class JsonWebToken
+
+  JWT_SECRET = ENV.fetch('JWT_SECRET_KEY') { raise "JWT_SECRET_KEY is missing in environment variables" }
+
   def self.encode(payload)
-    JWT.encode(payload, ENV.fetch('JWT_SECRET_KEY', nil), 'HS256')
+    JWT.encode(payload, JWT_SECRET_KEY, nil), 'HS256')
   end
 
   def self.decode(token)
-    JWT.decode(token, ENV.fetch('JWT_SECRET_KEY', nil), true, { algorithm: 'HS256' }).first
+    JWT.decode(token, JWT_SECRET_KEY, nil), true, { algorithm: 'HS256' }).first
   rescue
     nil
   end
