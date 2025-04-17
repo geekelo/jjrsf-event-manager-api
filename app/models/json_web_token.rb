7e9f15f854312d1ee85eec9d67c5c678995dcb50
Eloghene <eloghene@bananacrystal.com>
2025-04-17 14:48:21 +0100

    Troubleshoot bug

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,11 +1,11 @@
 class JsonWebToken
-    def self.encode(payload)
-      JWT.encode(payload, ENV.fetch('JWT_SECRET_KEY', nil), 'HS256')
-    end
-  
-    def self.decode(token)
-      JWT.decode(token, ENV.fetch('JWT_SECRET_KEY', nil), true, { algorithm: 'HS256' }).first
-    rescue
-      nil
-    end
+  def self.encode(payload)
+    JWT.encode(payload, ENV.fetch('JWT_SECRET_KEY', nil), 'HS256')
   end
+
+  def self.decode(token)
+    JWT.decode(token, ENV.fetch('JWT_SECRET_KEY', nil), true, { algorithm: 'HS256' }).first
+  rescue
+    nil
+  end
+end
