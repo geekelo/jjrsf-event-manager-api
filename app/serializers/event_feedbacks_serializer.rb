3b894a4cdd12185da2b4130bb86917598e95f7d4
Eloghene <eloghene@bananacrystal.com>
2025-04-19 09:55:20 +0100

    Fix name error

--- app/serializers/event_feedbacks_serializer.rb
+++ app/serializers/event_feedbacks_serializer.rb
@@ -1,3 +1,3 @@
-class EventFeedbackSerializer < ApplicationRecord
+class EventFeedbackSerializer < ActiveModel::Serializer
   attributes :id, :testimony, :name, :review, :foundation_event_id, :created_at, :updated_at
 end
