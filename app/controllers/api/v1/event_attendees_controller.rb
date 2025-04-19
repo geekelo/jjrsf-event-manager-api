1458445a24a8d0807ca23f6c7c1b8ac81f5dd889
Eloghene <eloghene@bananacrystal.com>
2025-04-19 07:57:27 +0100

    Fix bugs

--- app/controllers/api/v1/event_attendees_controller.rb
+++ app/controllers/api/v1/event_attendees_controller.rb
@@ -1,4 +1,4 @@
-class EventAttendeesController < ApplicationController
+class Api::V1::EventAttendeesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :update]
   before_action :set_event, only: [:index, :create]
   before_action :set_attendee, only: [:update]
