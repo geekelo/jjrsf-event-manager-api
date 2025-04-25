3dcef1f5060134346f0d2a3d56dcf1a0b9f872cb
Eloghene <eloghene@bananacrystal.com>
2025-04-25 10:25:01 +0100

    Add visibility

--- app/serializers/foundation_event_serializer.rb
+++ app/serializers/foundation_event_serializer.rb
@@ -1,5 +1,5 @@
 class FoundationEventSerializer < ActiveModel::Serializer
   attributes :id, :unique_id, :name, :description, :start_date, :end_date, :online, :onsite, :location, :status,
-  :registration_deadline, :evaluation, :image_url, :created_at, :updated_at
+  :registration_deadline, :evaluation, :visibility, :image_url, :created_at, :updated_at
 
 end
