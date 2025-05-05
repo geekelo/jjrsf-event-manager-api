40f9fe08967e21fe9cbc7cedbc820e6ee625a9b9
Eloghene <eloghene@bananacrystal.com>
2025-05-05 22:39:36 +0100

    Add custom frontend url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:3000', 'http://localhost:5173', 'https://jjrsf-event-manager-admin.vercel.app',
-    'https://jjrsf-event-manager-guest.vercel.app'] # Add your frontend origin(s)
+    origins [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://jjrsf-event-manager-admin.vercel.app',
+    'https://jjrsf-event-manager-guest.vercel.app',
+    'https://programsportal.jjrsf.org',
+    'https://programs.jjrsf.org',
+  ] # Add your frontend origin(s)
 
     resource '/api/*',
       headers: :any,
