6cb58b7ff65903e149d7b081106d463df5093a9b
Eloghene <eloghene@bananacrystal.com>
2025-05-25 16:58:38 +0100

    Fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,8 +5,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     'http://localhost:5173',
     'https://jjrsf-event-manager-admin.vercel.app',
     'https://jjrsf-event-manager-guest.vercel.app',
-    'https://programsportal.jjrsf.org',
-    'https://programs.jjrsf.org',
+    # 'https://programsportal.jjrsf.org',
+    # 'https://programs.jjrsf.org',
   ] # Add your frontend origin(s)
 
     resource '/api/*',
